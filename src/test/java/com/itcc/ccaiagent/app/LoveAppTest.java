37bfb26edde0b3437736151b62f1ca35284a7791
Leocc001 <2771169835@qq.com>
2026-02-09 14:28:53 +0800

    新增恋爱大师应用app

---
+++ src/test/java/com/itcc/ccaiagent/app/LoveAppTest.java
@@ -0,0 +1,32 @@
+package com.itcc.ccaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+    @Resource
+    private LoveApp loveApp;
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+
+        String message = "你好，我是程序员CC";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);//断言不是空
+
+        message = "我想让另一半(leoo)更爱我";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
